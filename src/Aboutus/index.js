c2493d9c43cea3c466186ccdfa1691992d843730
Mac <mac@Usmans-Mac.local>
2024-01-02 15:37:40 +0500

    update

---
+++ src/Aboutus/index.js
@@ -0,0 +1,26 @@
+import './styles.css';
+import banner from './../assests/Banner_orient.webp'
+import logo from './../assests/Care-logo.avif'
+
+function Index() {
+    return (
+        <div className="App">
+            <img src={banner} className="App-logo" />
+            <div className='logoContainer'>
+                <img src={logo} className='logo' alt="logo" />
+            </div>
+            <div className='discriptionContainer'>
+                <p className='discription'>
+                    OrientCare is committed to provide its valued customers the ultimate care and comfort through its range of technologically advanced and innovative consumer electronics. The core of our product development philosophy is based on detailed research and consumer insights to offer enhanced lifestyle and peace of mind at every aspect of product usage.
+                    <br /><br />
+                    OrientCare having a global perspective, offers durable, cost effective and energy efficient products to a range of audiences and markets. These products are duly certified and tested conforming to quality & performance standards as required in every region where these are being marketed.
+                    <br /><br />
+                    Our ultimate goal is to maintain the highest quality standards and uphold our brand’s core essence of care and innovation in terms of technology, research, and development. OrientCare offers three types of air-conditioning solutions; Mini Split Inverter Heat Pumps, Multi-zone Inverter Heat Pumps and Air Handler Units Inverter Heat Pumps offering a range of innovative features and technologies. With OrientCare, you can trust your choice that not only aligns with your needs, but is also backed by a reliable and efficient aftersales services and availability of parts required.
+
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Index;
---
+++ src/ContactUs/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Typography, Grid, Checkbox, FormControlLabel, TextField, Button } from '@mui/material';
+import logo from './../assests/Care-logo.avif';
+import Box from '@mui/material/Box';
+import InputFields from './inputFields';
+
+function Index() {
+    return (
+        <div>
+            <Grid item textAlign="center" sx={{ marginTop: '40px' }}>
+                <img src={logo} alt="Logo" style={{ maxWidth: '160px' }} />
+                <Typography variant='h6' sx={{ fontWeight: 700, color: '#343538' }}>CONTACT US</Typography>
+            </Grid>
+            <Grid container spacing={2} sx={{ background: '#f2f2f2', marginTop: 12 }} justifyContent="space-between" flexWrap='wrap' alignItems="center">
+                <Grid item sx={{ marginX: 10, marginBottom: '5%', }}>
+                    <Typography sx={{
+                        color: '#595959',
+                        fontSize: '20px',
+                        fontWeight: '600',
+                        marginTop: '5%',
+                        marginX: 1.5
+                    }}>
+                        <p style={{ marginBottom: '10%' }}>  Email Us</p>
+                    </Typography>
+                    <Box
+                        component="form"
+                        sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' }, }}>
+                        <InputFields title={"Your Name"} />
+                        <InputFields title={"Your email"} />
+                        <InputFields title={"Subject"} />
+                        <InputFields multi title={"Your Message"} />
+                        <FormControlLabel sx={{
+                            width: '40em',
+                            fontWeight: 200,
+                            color: '#8a8a8a',
+                            fontSize: '1rem',
+                            lineHeight: '1.5',
+                        }} control={<Checkbox />} label="I consent to having this website store my submitted information so they can respond to 
+                                my inquiry.See our privacy policy to learn more how we use data." />
+                    </Box>
+                    <Button sx={{ marginY: '10px' }} variant="contained" color="success">
+                        Success
+                    </Button>
+                </Grid>
+                <Grid item sx={{ marginX: 10 }}>
+                    <Typography>
+                        dsfvsdf
+                    </Typography>
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
+
+export default Index;
